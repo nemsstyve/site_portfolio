d0b06f439615cb8dbcd205430594f0a45ea767cf
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-22 22:02:12 +0200

    site_portfolio

--- public/assets/vendor/php-email-form/php-email-form.php
+++ public/assets/vendor/php-email-form/php-email-form.php
@@ -1,19 +1,8 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "name@example.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php include("/site_portfolio/public/index.html"); ?>
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
+    <div class='sucess'>
+            <h3>Vous avez envoyer votre message avec succés.</h3>
+            <p>Cliquez ici pour retourner <a href="/site_portfolio/public/index.html">à la page d'accueil</a></p>
+    </div>
+
+<?php include("assets/inc/footer.inc.php"); ?>
