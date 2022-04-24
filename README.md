b3f90a45e2190bccb104e74e823ae9cb80551153
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-24 20:46:21 +0200

    site_portfolio

--- README.md
+++
@@ -1,7 +0,0 @@
-# Site_Portfolio
-
-Depot git du site Portfolio.
-
-# To do list
-
-Reste à héberger le site 
---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,8 @@
+<?php include("/site_portfolio/public/index.html"); ?>
+
+    <div class='sucess'>
+            <h3>Vous avez envoyer votre message avec succés.</h3>
+            <p>Cliquez ici pour retourner <a href="/site_portfolio/public/index.html">à la page d'accueil</a></p>
+    </div>
+
+<?php include("assets/inc/footer.inc.php"); ?>
